244d11b1e6d97f9fa7909b760505dfd86550093e
Sweetyshadow <Shadow_8890@126.com>
2018-05-27 21:57:37 +0800

    intro added

---
+++ front-end/src/components/ADI.vue
@@ -0,0 +1,38 @@
+<template>
+<div class="wrap">
+    <intro-title i-title="ADI杯新生信息知识竞赛"></intro-title>
+    <div class="content">
+        <div class="article">
+            <h4>赛事时间</h4>
+            <p>ADI杯新生信息知识竞赛在【秋季学期】举行，在新生入学后会有相关的宣传工作，通常期中考试前结束所有工作。               
+            </p>
+            <h4>赛事简介</h4>
+            <p>
+               清华大学ADI杯新生信息知识竞赛至今已成功举办15届。该活动是由清华大学信息学院四系科协（电子系，计算机系，自动化系，软件学院）合办的一年一届的全校活动。活动旨在向新生普及信息知识，发现人才。这项活动面向全校3000多名新生，尤其是1000多名信息学院的新同学，已在校内产生了良好而广泛的影响。 
+            </p>
+            <p>
+                全校初赛采取统一笔试环节，选取五到七支队伍进入决赛。电子系系内初赛及全校决赛试题以powerpoint形式，有必答题、抢答题、视频题、女生题、人气题、你说我猜题、渐进抢答题和风险题等环节，均为现场作答。
+            </p>
+            <h4>往年情况</h4>
+            <p>
+               往年奖品包括小米手环、极路由、蓝牙音箱、扫地机器人、打印机、移动硬盘等等，同时观赛亦有机会获得观众奖。 
+            </p>
+            <img src='../../static/img/ADI-1.jpg'/>
+            <p class='ia'>第十五届决赛照片</p>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+import '../../static/css/article.css';
+import IntroTitle from './IntroTitle';
+export default {
+  name: 'ADI',
+  components: { IntroTitle },
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
