51f4edc4c6024db3065eceab9e434e86d7547511
Sweetyshadow <Shadow_8890@126.com>
2018-05-28 00:31:13 +0800

    fix a align problem

--- front-end/src/components/ts.vue
+++ front-end/src/components/ts.vue
@@ -10,10 +10,17 @@
             <p>“队式程序设计大赛”是由清华大学电子系科协举办的一项经典赛事，是一个组队参加的对抗性策略程序设计比赛。比赛主题往往基于某款经典电子游戏，设计出的全新规则，邀请选手组队设计出更加完善和智能的游戏策略，并用高效的程序代码实现。比赛往往以双方AI对战的形式进行，决赛时还会使用3D界面向全场播送比赛实况，对抗性和观赏性极强，为清华大学内一年一度的程序设计盛事。
             </p>
             <h4>赛事精彩回顾</h4>
-            <img src='../../static/img/ts-1.png'/>
-            <p class='ia'>队式十六——深蓝(Deep Blue)</p>
-            <img src='../../static/img/ts-2.png'/>
-            <p class='ia'>队式十七——Stellar craft</p>
+            <div class="img-wrap">
+                <img src='../../static/img/ts-1.png'/>
+                <p class='ia'>队式十六——深蓝(Deep Blue)</p>
+            </div>
+            <div class="img-wrap">
+                <img src='../../static/img/ts-2.png'/>
+                <p class='ia'>队式十七——Stellar craft</p>
+            </div>
+            <div class="img-wrap">
+                <img src='../../static/img/ts-3.png'/>
+            </div>
         </div>
     </div>
 </div>
